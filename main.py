1dc2865b9924f88f3c57c94c8857218bc415ec30
kkordik <99617240+Kkordik@users.noreply.github.com>
2023-02-26 10:07:00 +0100

    main.py

--- main.py
+++ main.py
@@ -2,16 +2,11 @@ import asyncio
 from run_bot import dp
 from main_interface.start_cmd import register_main_start_cmd
 from inline_interface.inline_query import register_inline_query_handler
-# from main_interface.temporar import register_get_photo_id
 
 
 async def main(_loop):
-    # Register handlers
-    dp.stop_polling()
     register_main_start_cmd(dp)
     register_inline_query_handler(dp)
-    #register_get_photo_id(dp)
-
     await dp.start_polling()
 
 
